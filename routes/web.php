ffb4ffd355d4a59894bdf6046313f0cd6a326aa8
armanmasangkay <vss.arman@gmail.com>
2021-02-12 11:35:10 +0800

    moved dashboard to admin groups route

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', function () {
-    return view('pages.dashboard');
-});
+
 
 //ADMIN ROUTES
 Route::prefix('admin')->group(function(){
@@ -29,9 +27,13 @@ Route::prefix('admin')->group(function(){
     Route::post('/register-customer', [RegisterCustomerController::class, 'store']);
     Route::get('/customer-lists', [ViewCustomerController::class, 'index'])->name('view_customers');
     Route::get('/transactions/new', [TransactionController::class, 'index'])->name('new_transaction');
-    
+
     Route::get('/login', function () {
         return view('layout.login');
+    });
+
+    Route::get('/', function () {
+        return view('pages.dashboard');
     });
 });
 
